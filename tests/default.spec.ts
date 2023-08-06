cd40798e50f961a49356c05a187aa81a01c912ae
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-08-06 14:51:31 +0200

    Disable h1 test

--- tests/default.spec.ts
+++ tests/default.spec.ts
@@ -1,12 +1,12 @@
-import { test, expect,  } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('returns status code 200', async ({ page }) => {
   const response = await page.goto('/');
   expect(response?.status()).toBe(200);
 })
 
-test('has h1', async ({ page }) => {
-  await page.goto('/');
-  const h1 = await page.locator('h1');
-  await expect(h1).toBeVisible();
-});
+// test('has h1', async ({ page }) => {
+//   await page.goto('/');
+//   const h1 = await page.locator('h1');
+//   await expect(h1).toBeVisible();
+// });
