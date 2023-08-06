b103c0adf054dbea0215167b7c7f54ca5dc85469
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-08-06 14:27:35 +0200

    Multiple urls

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# End-to-end testing suite with Playwright
+
+- Run scheduled tests across multiple projects with Github Actions.
+  
+
+## Local installation
+
+	pnpm install
+
+## Run tests locally
+
+	pnpm run test
+	pnpm test -- --trace on
+	npx playwright show-report
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,77 +1,55 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const defaultUse = {
+  ...devices['Desktop Chrome'],
+};
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+const defaultTests = ['**/default.spec.ts'];
+
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  use: {
     trace: 'on-first-retry',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'Personal Website',
+      use: {
+        ...defaultUse,
+        baseURL: 'https://florian.geierstanger.org/',
+      },
+      testMatch: defaultTests,
     },
-
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: 'Lernhausfilm',
+      use: {
+        ...defaultUse,
+        baseURL: 'https://lernhausfilm.de/',
+      },
+      testMatch: defaultTests,
     },
-
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: 'Muenchen Plantscht',
+      use: {
+        ...defaultUse,
+        baseURL: 'https://muenchen-plantscht.vercel.app/',
+      },
+      testMatch: defaultTests,
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'Daily Bingo',
+      use: {
+        ...defaultUse,
+        baseURL: 'https://daily-bingo.vercel.app/',
+      },
+      testMatch: defaultTests,
+    },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
